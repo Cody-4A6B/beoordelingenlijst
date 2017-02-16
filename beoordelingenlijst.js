55ef3428fe3022c075d4ef497fc6f3490e1cbef4
Cody-4A6B <490248@edu.rocmn.nl>
2017-02-16 09:17:43 +0100

    bestanden aangepast

--- beoordelingenlijst.js
+++ beoordelingenlijst.js
@@ -13,3 +13,19 @@ function rekenCijfer() {
         document.getElementById('resultaat').innerHTML += cijfer + " is ongeldig, geef resultaat tussen 0 en 100" + '<br>';
     }
 }
+
+function rekenOordeel() {
+    var oordeel = document.getElementById('input2').value;
+
+    if (oordeel >= 0 && oordeel < 50) {
+        document.getElementById('resultaat').innerHTML += "De beoordeling is onvoldoende, want het cijfer is " + oordeel + '<br>';
+    } else if (oordeel >= 50 && oordeel < 60) {
+        document.getElementById('resultaat').innerHTML += "De beoordeling is matig, want het cijfer is " + oordeel + '<br>';
+    } else if (oordeel >= 60 && oordeel < 75) {
+        document.getElementById('resultaat').innerHTML += "De beoordeling is voldoende, want het cijfer is " + oordeel + '<br>';
+    } else if (oordeel >= 75 && oordeel <= 100) {
+        document.getElementById('resultaat').innerHTML += "De beoordeling is goed, want het cijfer is " + oordeel + '<br>';
+    } else {
+        document.getElementById('resultaat').innerHTML += cijfer + " is ongeldig, geef resultaat tussen 0 en 100" + '<br>';
+    }
+}
